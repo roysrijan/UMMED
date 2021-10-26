a604e58f5714e98d19c4db7fb5931dd9cf626791
Amit Karmakar <amitkarmakar.dev@gmail.com>
2021-10-26 17:01:59 +0530

    Fix header menu hide show

--- src/app/borrower/loan-search/loan-search.component.ts
+++ src/app/borrower/loan-search/loan-search.component.ts
@@ -1,20 +1,27 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http'
+import { Component } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
 
 @Component({
   selector: 'loan-search',
   templateUrl: './loan-search.component.html',
-  styleUrls: ['./loan-search.component.scss']
+  styleUrls: ['./loan-search.component.scss'],
 })
 export class LoanSearchComponent {
-  title = 'ummed';
-  loan: any = 'Medical';
-  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) {}
-  loanType(e: any){
+  title = 'ummed'
+  loan: any = 'Medical'
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
+
+  loanType(e: any) {
     this.loan = e.target.value
   }
-  loanSearch(){
-    this.router.navigate(['../search-result/'+this.loan],{relativeTo: this.route});
+  loanSearch() {
+    this.router.navigate(['../search-result/' + this.loan], {
+      relativeTo: this.route,
+    })
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,37 +1,34 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core'
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'sign-in',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  title = 'ummed';
-  role = 'Borrower';
-  form: FormGroup = new FormGroup({});
+  title = 'ummed'
+  role = 'Borrower'
+  form: FormGroup = new FormGroup({})
   constructor(private router: Router, private fb: FormBuilder) {}
-  ngOnInit(){
-  this.form = this.fb.group({
-    user_id: new FormControl(),
-    password: new FormControl(),
-    role: new FormControl(),
-  });   
-  }  
-  login(){
-    sessionStorage.setItem('user', JSON.stringify(this.form.value));
-    if(this.form.value.role=='Admin'){
-      this.router.navigate(['/signup']);
-    }
-    else if(this.form.value.role=='Borrower'){
-      this.router.navigate(['/borrower/loan-search']);
-    }
-    else if(this.form.value.role=='Retail Lender'){
-      this.router.navigate(['/retail-lender'])
-    }
-    else {
-      this.router.navigate(['/lender/new-applications'])
+  ngOnInit() {
+    this.form = this.fb.group({
+      user_id: new FormControl(),
+      password: new FormControl(),
+      role: new FormControl(),
+    })
+  }
+  login() {
+    sessionStorage.setItem('user', JSON.stringify(this.form.value))
+    if (this.form.value.role == 'Admin') {
+      window.location.href='/signup';
+    } else if (this.form.value.role == 'Borrower') {
+      window.location.href='/borrower/loan-search';
+    } else if (this.form.value.role == 'Retail Lender') {
+      window.location.href='/retail-lender';
+    } else {
+      window.location.href='/lender/new-applications';
     }
   }
 }
