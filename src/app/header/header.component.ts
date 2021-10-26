186394014f5f45995e9f7e9ab97ce785fb91444a
Aayush Jain <ayush8989049912@gmail.com>
2021-10-26 11:09:57 +0530

    Added Language toggle button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+  regionalLanguage: boolean = false;
+
+  onRegionalLanguageClick(value: string) {
+    this.regionalLanguage = !this.regionalLanguage;
+    if(value == 'Bengali')
+    this.router.navigate(["/borrower/loan-search-regional"]);
+    else this.router.navigate(["/borrower/loan-search"]);
   }
 
 }
